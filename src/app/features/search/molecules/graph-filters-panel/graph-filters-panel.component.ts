02d4a62774b248c11e09a829c861589cf6d4c2ca
Ori Gritzman <ori.gritzman@gmail.com>
2026-02-12 01:30:55 +0200

    right side layout

--- src/app/features/search/molecules/graph-filters-panel/graph-filters-panel.component.ts
+++ src/app/features/search/molecules/graph-filters-panel/graph-filters-panel.component.ts
@@ -103,8 +103,14 @@ export class GraphFiltersPanelComponent {
     const date = new Date(timestamp);
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear()).slice(-2);
+    return `${day}/${month}/${year}`;
+  }
+
+  public formatTime(timestamp: number): string {
+    const date = new Date(timestamp);
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    return `${day}/${month} ${hours}:${minutes}`;
+    return `${hours}:${minutes}`;
   }
 }
