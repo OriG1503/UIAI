a31b8c3d9655821bf082332b6522a292424b7479
Ori Gritzman <ori.gritzman@gmail.com>
2026-02-12 00:53:21 +0200

    cahnged graph colors

--- src/app/features/search/constants/graph.constants.ts
+++ src/app/features/search/constants/graph.constants.ts
@@ -5,17 +5,17 @@ export const NODE_SIZE_DEFAULT = 6;
 export const EDGE_SIZE_MIN = 1;
 export const EDGE_SIZE_MAX = 6;
 
-export const NODE_COLOR_DEFAULT = '#6a859d';
+export const NODE_COLOR_DEFAULT = '#03153a';
 export const NODE_COLOR_SELECTED = '#e74b3b';
 export const NODE_COLOR_NEIGHBOR = '#e74b3b';
 export const NODE_COLOR_DIMMED = '#9a9a9aB3';
 export const NODE_COLOR_HOVER = '#e74b3b';
-export const NODE_GAP_COLOR = '#e8edf5';
+export const NODE_GAP_COLOR = '#d9d9d9';
 
 export const NODE_BORDER_RATIO = 0.05;
 export const NODE_GAP_RATIO = 0.06;
 
-export const EDGE_COLOR_DEFAULT = '#6a859d';
+export const EDGE_COLOR_DEFAULT = '#03153a';
 export const EDGE_COLOR_SELECTED = '#e74b3b';
 export const EDGE_COLOR_HOVER = '#e74b3b';
 export const EDGE_COLOR_DIMMED = '#9a9a9aB3';
@@ -26,7 +26,7 @@ export const LABEL_COLOR_DIMMED = 'rgba(0, 0, 0, 0.7)';
 export const LABEL_FONT_FAMILY = 'Assistant';
 export const LABEL_FONT_SIZE = 12;
 export const LABEL_STROKE_WIDTH = 3;
-export const LABEL_STROKE_COLOR = '#ffffff';
+export const LABEL_STROKE_COLOR = '#d9d9d9';
 export const LABEL_RENDERED_SIZE_THRESHOLD = 0;
 export const LABEL_CLICK_RADIUS = 30;
 export const EDGE_LABEL_ZOOM_THRESHOLD = 1.5;
