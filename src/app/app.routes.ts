b7e371c9af9dfb33f2fd5b8f717bdaf5024bdb1d
Ori Gritzman <ori.gritzman@gmail.com>
2026-02-11 16:43:19 +0200

    changed basic app layout location and naming

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'search/list', pathMatch: 'full' },
   {
-    path: 'search',
-    loadChildren: () =>
-      import('./features/search/search.routes').then((m) => m.SEARCH_ROUTES),
-  },
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: 'search',
+        loadChildren: () => import('./features/search/search.routes').then((m) => m.SEARCH_ROUTES)
+      },
+      { path: '', redirectTo: 'search/list', pathMatch: 'full' }
+    ]
+  }
 ];
---
+++ src/app/layouts/app-layout/app-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from '../../features/search/organisms/search-bar/search-bar.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [RouterOutlet, SearchBarComponent],
+  templateUrl: './app-layout.component.html',
+  styleUrl: './app-layout.component.scss'
+})
+export class AppLayoutComponent {}
