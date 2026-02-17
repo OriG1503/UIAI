8df0a339e044da2906242efc1c70a5cd2fb7af5a
Ori Gritzman <ori.gritzman@gmail.com>
2026-02-17 11:49:33 +0200

    created a mail content loader

---
+++ src/app/features/mail-content/components/atoms/content-skeleton/content-skeleton.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-skeleton',
+  standalone: true,
+  templateUrl: './content-skeleton.component.html',
+  styleUrl: './content-skeleton.component.scss'
+})
+export class ContentSkeletonComponent {}
